ff5630901eeebb6dcf0329f66998d25f58c0b937
zEntry <109413832+zEntry6@users.noreply.github.com>
2025-05-28 22:07:16 +0700

    update env

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui i
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCtSttYhYWVhbQh-KubZI-MXrgOH8Y9O00",
-  authDomain: "portofolio-90cb6.firebaseapp.com",
-  projectId: "portofolio-90cb6",
-  storageBucket: "portofolio-90cb6.firebasestorage.app",
-  messagingSenderId: "461684922281",
-  appId: "1:461684922281:web:eae0b6623b01bdd5d556b8",
-  measurementId: "G-QY2YRSH6Q0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
